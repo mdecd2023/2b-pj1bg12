6ab47cc5440d8930138f69c0bf549dc1c45f3a3d
41023242 <41023242@gm.nfu.edu.tw>
2023-03-09 15:24:24 +0800

    組員

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 41023202梁琇婷 \n 41023242廖旭宏', 'tags': '', 'url': 'About.html'}]};
